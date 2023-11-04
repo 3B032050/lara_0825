50607cdc8eac4eaeaf5b30cf0292ccf737364101
黃文輝 <antory040512@gmail.com>
2023-11-04 19:06:14 +0800

    改用save的方式儲存資料

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $post=new post();
+    $post->title='test title';
+    $post->content = 'test content';
+    $post->save();
 });
