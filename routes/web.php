d15cea385cd75aefc7c944bf81b67bec88f6f21d
黃文輝 <antory040512@gmail.com>
2023-11-28 14:03:13 +0800

    取得單一筆貼文由大致小排列

--- routes/web.php
+++ routes/web.php
@@ -78,8 +78,11 @@ Route::get('/', function () {
     /*
     $featuredPosts = Post::where('is_feature',1)->get();
     dd($featuredPosts);*/
+    /*
     $fourthPost = Post::find(4);
-    dd($fourthPost);
+    dd($fourthPost);*/
+    $lastPost = Post::orderBy('id','DESC')->first();
+    dd($lastPost);
 
 
 
